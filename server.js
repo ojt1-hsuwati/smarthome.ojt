ffd906d490e4be24a3cb953f857c00d65ed36bf2
ojt1-hsuwati <det.ojt2023@gmail.com>
2024-11-26 15:47:20 +0630

    192.168.0.118:3000

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: 'http://localhost:3000'
+    origin: 'http://192.168.0.118:3000'
   }
 });
 
